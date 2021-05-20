88e40fb448be67fea5be2329a42a275dd6c8b353
Emily Foley <efoley326@gmail.com>
2021-05-19 23:44:05 -0400

    lots of paths to nowhere

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-document.querySelector(".buttons").addEventListener('click', function(event){
-    console.log(event.target)
- })
+let decimal = document.getElementById(“dot”);
+let clear = document.getElementById(“clear”);
+let display = document.getElementById(“display-window”);
+let buttonnumbers = document.getElementsByClassName(“buttons”);
+let operators = document.getElementsByClassName(“functions”);
 
- 
+for (let i = 0; i < buttonnumbers.length; i++) {
+    buttonnumbers[i].addEventListener(‘click’,  ) 
+}
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener(‘click’, );
+}
+// gotta add functions after click? I think?
+
+
+// making jobs for operators -- array.push
+
+
+
+//sending function output to display
+let outputDisplay = document.createElement('h1')
+let headerText= 
+document.create TextNode('//function output') 
+console.log (outputDisplay)
+
+messageDive.innerText=Event
+console.log ('//button-value', Event.target.innerText)
+cont output= document.querySelector ('output')
+output.innerText +=Event.target.innerText
+
+
